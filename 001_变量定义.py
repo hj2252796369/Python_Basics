f943e9725d22786d1fcdddaced00e469a132b199
hujie <2252796369@qq.com>
2023-08-15 22:04:54 +0800

    Initial commit

---
+++ 001_变量定义.py
@@ -0,0 +1,18 @@
+# ctrl + ?  注释
+
+# 不是用变量
+print('今天天气很好， 又下雨了')
+print('今天天气很好， 又下雨了')
+print('今天天气很好， 又下雨了')
+print('今天天气很好， 又下雨了')
+print('今天天气很好， 又下雨了')
+print('今天天气很好， 又下雨了')
+
+# 使用变量
+weather = "今天天气很好， 晴天了"
+print(weather + "aaaaaaa")
+print(weather)
+print(weather)
+print(weather)
+print(weather)
+print(weather)
